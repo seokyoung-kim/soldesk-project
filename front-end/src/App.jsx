ff009f7b758bb5f424795daec224a6fd100340ed
SeokYoung Kim <seokyoungkimm@gmail.com>
2020-07-16 15:57:09 +0900

    테스트 기능 구현

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import MediaCard from "./components/CardList/MediaCard";
+import CustomizedTabs from "./components/CardList/CustomizedTabs";
+import MyPage from "./pages/MyPage";
 
 const App = () => {
     return (
         <>
+        <CustomizedTabs />
         <MediaCard />
         <MediaCard />
-        <MediaCard />
+        
+        <MyPage />
         </>
     )
 };
--- front-end/src/pages/MyPage.jsx
+++ front-end/src/pages/MyPage.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import MainPagination from "../components/Common/MainPagination";
+import VerTab from "../components/CardList/VerTab";
 
 const MyPage = () => {
     return (
         <>
-        
+            <MainPagination/>
+            <VerTab />
         </>
     )
 }
