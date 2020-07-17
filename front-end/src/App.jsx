4cc4e62cc5f7f29ce6058041e9c85b2c0db63b68
seokyoung-kim <seokyoungkimm@gmail.com>
2020-07-17 16:20:03 +0900

    폴더 구조 변경 및 Mainpage 테스트

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,30 +1,10 @@
 import React from "react";
-import MediaCard from "./components/CardList/MediaCard";
-import CustomizedTabs from "./components/CardList/CustomizedTabs";
-import MyPage from "./pages/MyPage";
-import SelectCombo from "./components/Selector/SelectCombo";
-import LoginField from "./components/Field/LoginField";
-import PwdField from "./components/Field/PwdField";
-import InputMulti from "./components/Common/InputMulti";
-import InputOne from "./components/Common/InputOne";
-import CustomizedButtons from "./components/Common/CustomizedButtons";
-
-
-
+import MainPage from "./pages/MainPage";
 
 const App = () => {
     return (
         <>
-        <CustomizedTabs />
-        <MediaCard />
-        <MediaCard />
-        <MyPage />
-        <SelectCombo/>
-        <LoginField/>
-        <PwdField/>
-        <InputMulti/>
-        <InputOne/>
-        <CustomizedButtons/>
+        <MainPage />
         </>
     )
 };
--- front-end/src/pages/MainPage.jsx
+++ front-end/src/pages/MainPage.jsx
@@ -1,7 +1,68 @@
-import React from "react";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
 
-const MainPage = () => {
+import Header from './Header';
+import MainFeaturedPost from './MainFeaturedPost';
+import FeaturedPost from './FeaturedPost';
+import Main from './Main';
+import Footer from './Footer';
 
+const useStyles = makeStyles((theme) => ({
+  mainGrid: {
+    marginTop: theme.spacing(3),
+  },
+}));
+
+
+const mainFeaturedPost = {
+  title: 'For Developers',
+//   description:
+//     "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
+  image: 'https://source.unsplash.com/random',
+  imgText: 'main image description',
+//   linkText: 'Continue reading…',
+};
+
+const featuredPosts = [
+  {
+    title: 'Featured post',
+    date: 'Nov 12',
+    description:
+      'This is a wider card with supporting text below as a natural lead-in to additional content.',
+    image: 'https://source.unsplash.com/random',
+    imageText: 'Image Text',
+  },
+  {
+    title: 'Post title',
+    date: 'Nov 11',
+    description:
+      'This is a wider card with supporting text below as a natural lead-in to additional content.',
+    image: 'https://source.unsplash.com/random',
+    imageText: 'Image Text',
+  },
+];
+
+export default function MainPage() {
+  const classes = useStyles();
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <Header title="개발세발" />
+        <main>
+          <MainFeaturedPost post={mainFeaturedPost} />
+          <Grid container spacing={4}>
+            {featuredPosts.map((post) => (
+              <FeaturedPost key={post.title} post={post} />
+            ))}
+          </Grid>
+        </main>
+      </Container>
+      <Footer/>
+    </React.Fragment>
+  );
 }
-
-export default MainPage;
